f640fa33b475c14f2eb086979975d985d1c6f8d8
Maria Luiza <maria.lmm.975@gmail.com>
2022-05-20 21:13:30 -0300

    feat: add endpoit GET /courses/:idCourse

--- src/controllers/coursesController.js
+++ src/controllers/coursesController.js
@@ -1,3 +1,5 @@
+import { ObjectId } from "mongodb";
+
 import db from "./../db.js"; 
 
 export async function getCourses(_req, res){
@@ -8,6 +10,27 @@ export async function getCourses(_req, res){
         console.log(error);
         res.status(500).send({
             message: 'Error getting courses',
+            detail: error
+        });
+    }
+}
+
+export async function getCourse(req, res) {
+    const {idCourse} = req.params;
+
+    try{
+        const course = await db.collection("courses").findOne({_id: new ObjectId(idCourse)});
+
+        if(course){
+            res.send(course);
+        } else{
+            res.sendStatus(404);
+        }
+
+    } catch(error){
+        console.log(error);
+        res.status(500).send({
+            message: 'Error getting course',
             detail: error
         });
     }
